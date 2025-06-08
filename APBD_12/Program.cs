312a4b8b3520938a00c28701fe13763b7742c19f
Maksym Rubtsov <maksrub04@gmail.com>
2025-06-08 11:22:50 +0200

    updated

--- APBD_12/Program.cs
+++ APBD_12/Program.cs
@@ -1,4 +1,5 @@
-﻿using APBD_12.Data;
+﻿using System.Reflection;
+using APBD_12.Data;
 using APBD_12.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
@@ -8,19 +9,41 @@ using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Debugging output
+Console.WriteLine($"Current Directory: {Directory.GetCurrentDirectory()}");
+Console.WriteLine($"Content Root Path: {builder.Environment.ContentRootPath}");
+
+try
+{
+    var config = new ConfigurationBuilder()
+        .SetBasePath(builder.Environment.ContentRootPath)
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddEnvironmentVariables()
+        .Build();
+    
+    builder.Configuration.AddConfiguration(config);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"CONFIG LOAD ERROR: {ex.Message}");
+    throw;
+}
+
+var connectionString = builder.Configuration.GetConnectionString("Default") 
+    ?? throw new InvalidOperationException("Connection string 'Default' not found");
+
+Console.WriteLine($"Using Connection String: {connectionString}");
+
 builder.Services.AddDbContext<Apbd12Context>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+    options.UseSqlServer(connectionString));
 
 builder.Services.AddScoped<ITripService, TripService>();
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -28,9 +51,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
